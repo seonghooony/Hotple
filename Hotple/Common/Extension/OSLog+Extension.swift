3de61deabfdf99ad4347afa6950758560e415758
seonghooony <sh-k@kakao.com>
2023-06-07 10:59:31 +0900

    modify bugs

--- Hotple/Common/Extension/OSLog+Extension.swift
+++ Hotple/Common/Extension/OSLog+Extension.swift
@@ -13,7 +13,7 @@ extension OSLog {
     static let network = OSLog(subsystem: subsystem, category: "Network")
     static let debug = OSLog(subsystem: subsystem, category: "Debug")
     static let info = OSLog(subsystem: subsystem, category: "Info")
-    static let error = OSLog(subsystem: subsystem, category: "Error")
+    static let error = OSLog(subsystem: subsystem, category: "Error") 	
 }
 
 struct Log {
